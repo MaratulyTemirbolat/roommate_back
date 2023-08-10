bdada62bbdffc17acb5767136cc01393bc385958
Temirbolat <snake2009003@mail.ru>
2023-08-10 15:33:52 +0600

    Added message to the IsNonDeletedUser permission.

--- apps/auths/permissions.py
+++ apps/auths/permissions.py
@@ -1,10 +1,16 @@
+# Python
 from typing import Any
 
+# Rest Framework
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request as DRF_Request
 
 
 class IsNonDeletedUser(BasePermission):
+    """IsNonDeletedUser."""
+
+    message: str = "Вы не можете запрашивать данные, пока ваш аккаунт удалён."
+
     def has_permission(self, request: DRF_Request, view: Any) -> bool:
         """Handle request permissions."""
         return bool(
