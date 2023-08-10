35fa109e7cb89653f57790a4b0419be42531321d
Temirbolat <snake2009003@mail.ru>
2023-08-10 15:36:19 +0600

    Added function 'get_filled_params_dict' to get dictionary with required parameters.

--- apps/abstracts/tools.py
+++ apps/abstracts/tools.py
@@ -1,4 +1,9 @@
-from typing import Optional
+from typing import (
+    Optional,
+    Tuple,
+    Dict,
+    Any,
+)
 
 
 def conver_to_int_or_none(number: str = "") -> Optional[int]:
@@ -7,3 +12,17 @@ def conver_to_int_or_none(number: str = "") -> Optional[int]:
         return int(number)
     except ValueError:
         return None
+
+
+def get_filled_params_dict(
+    req_params: Tuple[str],
+    **query_params: Dict[str, Any]
+) -> Dict[str, Any]:
+    """Get filled dictioanary with the provided params."""
+    res_dict: Dict[str, Any] = {}
+
+    key: str
+    for key in query_params:
+        if key in req_params:
+            res_dict.setdefault(key, query_params[key][0])
+    return res_dict
